de0134e634dd61a8c794c75ab1dd76c415c655b8
John Wood <kb2vhn@gmail.com>
2026-08-11 07:56:43 -0400

    fix: remediate GO-2026-5970 dependency

--- go/platform/scripts/check.sh
+++ go/platform/scripts/check.sh
@@ -62,10 +62,10 @@ github.com/kr/pretty v0.3.0
 github.com/pmezard/go-difflib v1.0.0
 github.com/stretchr/objx v0.1.0
 github.com/stretchr/testify v1.11.1
-golang.org/x/mod v0.27.0
-golang.org/x/sync v0.17.0
-golang.org/x/text v0.29.0
-golang.org/x/tools v0.36.0
+golang.org/x/mod v0.37.0
+golang.org/x/sync v0.21.0
+golang.org/x/text v0.39.0
+golang.org/x/tools v0.47.0
 gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c
 gopkg.in/yaml.v3 v3.0.1
 MODULES
